484bd169e08ad51d4d4cb1073c226c680912685b
Takuma Konishi <takm2357msc@icloud.com>
2021-07-13 15:47:53 +0900

    Modified SQLALCHEMY_DATABASE_URL and Comment out db.create_all

--- main.py
+++ main.py
@@ -16,8 +16,10 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
+SQLALCHEMY_URL = 'sqlite:///blog.db'
+
 # #CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL"), 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
@@ -69,7 +71,7 @@ class Comment(db.Model):
     text = db.Column(db.Text)
 
 
-db.create_all()
+# db.create_all()
 
 
 def admin_only(f):
